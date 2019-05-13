03375c164ddc92c9608ee7282475832ad13ead31
MattIPv4 <matthew@cowley.org.uk>
2019-05-13 21:43:05 +0100

    Add comments to controls

--- controls.js
+++ controls.js
@@ -1,3 +1,23 @@
+/**
+ *  rgshw.github.io: The selector site for the projection tools used in the main hall.
+ *  <https://github.com/rgshw/rgshw.github.io/>
+ *  Copyright (C) 2018 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  You should have received a copy of the GNU Affero General Public License
+ *   along with this program. If not, please see
+ *   <https://github.com/rgshw/rgshw.github.io/blob/master/LICENSE> or <http://www.gnu.org/licenses/>.
+ **/
+/* https://jscompress.com/ */
+
+// Render the modal
 function showHelp(repo) {
     var modal = document.createElement("div");
     modal.style.position = "fixed";
@@ -45,6 +65,8 @@ function showHelp(repo) {
     document.body.appendChild(modal);
 }
 
+
+// Render the modal toggle
 function help(repo) {
     var button = document.createElement("a");
     button.style.position = "fixed";
@@ -71,6 +93,7 @@ function help(repo) {
     document.body.appendChild(button);
 }
 
+// Render the back/home button
 function home() {
     var button = document.createElement("a");
     button.style.position = "fixed";
@@ -98,6 +121,7 @@ function home() {
     document.body.appendChild(button);
 }
 
+// Trigger control rendering
 function controls(repo, showHome) {
     if (showHome) home();
     help(repo);
